d2efbb1b9f93ddb51bc5fc81119be23e48380e67
Zach Obront <zobront@gmail.com>
2022-06-20 14:44:20 -0500

    homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Zach Obront</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
